31301ec67f86e21a0d3fb1e06c4111a901aa27d2
Daniel Taylor <Daniel.Taylor7@britishgas.co.uk>
2023-05-23 20:36:39 +0100

    Building on skeleton functionality.

--- src/main/scala/app/app.scala
+++ src/main/scala/app/app.scala
@@ -9,19 +9,26 @@ case class Delivery(inputs: List[String] = List.empty[String]) {
     orderNumbers ::: List.range(1, inputs.length + 1)
   }
 
-  def calculateDeliveryTime(orderNumbers: List[Int]): List[Double] = {
+  def calculateDeliveryTime(orderNumbers: List[Int]): List[Int] = {
     val timeObject = Time()
     val packagingTime = timeObject.packaging
     val carryingTime = timeObject.carrying
     val postageTime = timeObject.posting
 
-    val result = orderNumbers.map(order => (order * packagingTime) + (order * carryingTime) + postageTime)
+    import math.ceil
+    val result = orderNumbers.map(order => ((order * packagingTime) + (order * carryingTime) + postageTime).ceil.toInt)
     result
+  }
+
+  def returnDeliveryTimes(deliveryTimes: List[Int]): Unit = {
+    for ((deliveryTime, index) <- deliveryTimes.zipWithIndex) {
+      println(s"Item $index has delivery time of: $deliveryTime days")
+    }
   }
 }
 
 object Main extends App {
-  val test = Delivery(List("TShirt", "Trousers"))
-  val testing_output = test.calculateDeliveryTime(test.calculateOrderNumbers())
-  println(testing_output)
+  val deliveryClass = Delivery(List("TShirt", "Trousers"))
+  val deliveryTimes = deliveryClass.calculateDeliveryTime(deliveryClass.calculateOrderNumbers())
+  val printDeliveryTimes = deliveryClass.returnDeliveryTimes(deliveryTimes)
 }
